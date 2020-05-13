f4bfffeb75cfe4550f20a54fb826fb61d78d73a4
Dmytro Malon <46137635+dieie32@users.noreply.github.com>
2020-05-14 01:17:42 +0300

    init

---
+++ src/assets/js/index.js
@@ -0,0 +1,15 @@
+let taskList = {
+  today: [
+    { task: "create TODO list", remind: "today", marked: false },
+    { task: "Eat apple", remind: "today", marked: true },
+  ],
+  tommorow: [
+    { task: "do homework", remind: "tommorow", marked: false },
+    { task: "Eat apple", remind: "tommorow", marked: true },
+  ],
+  nextWeek: [{}],
+  marked: [
+    { task: "Eat apple", remind: "today", marked: true },
+    { task: "Eat apple", remind: "tommorow", marked: true },
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["transform-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+};
